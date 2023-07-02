5e6d3cd75bf84fa2597faaad76c477bf528e395f
Xiangan He <xiangan9@gmail.com>
2023-07-02 15:14:26 -0400

    add timeout config to z3 static

--- lowlevel/src/backend/z3_static.rs
+++ lowlevel/src/backend/z3_static.rs
@@ -7,10 +7,13 @@ pub struct Z3Static {
 }
 
 impl Z3Static {
-    pub fn new() -> Result<Self, std::io::Error> {
+    pub fn new(timeout: &Option<u64>) -> Result<Self, std::io::Error> {
         Ok(Z3Static {
             ctx: unsafe {
                 let cfg = z3_sys::Z3_mk_config();
+                if let Some(timeout) = timeout {
+                    z3_sys::Z3_set_param_value(cfg, CString::new("timeout").unwrap().as_ptr(), timeout.to_string().as_ptr());
+                }
                 let ctx = z3_sys::Z3_mk_context_rc(cfg);
                 z3_sys::Z3_set_error_handler(ctx, None);
                 z3_sys::Z3_set_ast_print_mode(ctx, z3_sys::AstPrintMode::SmtLib2Compliant);
